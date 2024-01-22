bd3e31f77a06d5bebed247fce9e9b5e7032049d3
Abdul Khaleq <abdulkhaleq6008@gmail.com>
2024-01-22 22:10:13 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# CRM-application-with-Django" 
---
+++ static/js/app.js
@@ -0,0 +1,6 @@
+// Message timer 
+var message_timeout = document.getElementById("message-timer");
+
+setTimeout(function () {
+    message_timeout.style.display = "none";
+}, 5000);
